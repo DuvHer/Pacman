95074f74ddce2bc5927563c777dcf02711b0bdb1
Estudiante <Estudiante@10.203.3.187>
2016-11-22 16:06:51 -0500

    Nov 17-2

---
+++ src/pacman/Pacman.java
@@ -0,0 +1,34 @@
+package pacman;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Pacman extends JFrame {
+
+    public Pacman() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new tablero());
+        setTitle("muñecos");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setVisible(true);        
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                Pacman ex = new Pacman();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
